1e8c6e62d379a6aacb0cff31e047145709e44c94
Rubyv7 <rvaca60@stuy.edu>
2024-06-07 14:26:46 -0400

    +gitignore

--- py/w38_rand.py
+++ py/w38_rand.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python
-
 print('Content-type: text/html\n')
 
-from random import random
+import random
 
-print('hey' + str(random()))
+space = 0
 
+def roll():
+    x = random.randrange(6) + 1
+    return x
+
+print(roll())
+
+def spacer():
+    space = space + roll()
+    return space
+print(space)
